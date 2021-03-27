c228dcc0d671c655b6a3aa6742739d70627bb4b3
nick-kudryavtsev <nick_kudryavtsev@internet.ru>
2021-03-27 12:26:10 +0300

    Socket added

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*'
+    }
+});
 
 //  constants
 const PORT = 3001
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
+});
+
+//  начинаем работать с сокетом
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected.')
+    })
 });
 
 http.listen(PORT, () => {
